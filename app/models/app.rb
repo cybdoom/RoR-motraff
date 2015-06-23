a96b6058c9abe87d3ea7b6d320ead822992b1ab5
Eugene Narovlyansky <cybdoom@gmail.com>
2015-06-23 23:52:10 +0300

    Store saving added and improved

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,20 +2,34 @@ class App
   include Mongoid::Document
 
   STORES = [
-    :google_play, # = 0
-    :app_store    # = 1
+    :google_play,  # 0
+    :app_store,    # 1
+    :windows,      # 2
+    :black_berry   # 3
   ]
 
   embedded_in :link
 
-  field :market_id, type: String
-  field :url, type: String
+  field :store_id, type: String
+  field :app_url, type: String
   field :logo_url, type: String
   field :title, type: String
-  field :market, type: Integer
+  field :store, type: Integer
   field :publisher, type: String
 
-  validates :url, presence: true
+  validates :app_url, presence: true
   validates :title, presence: true, length: { minimum: 2, maximum: 256 }
-  validates :market, inclusion: { in: STORES }
+  validates :store, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than: STORES.size
+  }
+
+  def store= value
+    if value.is_a? Fixnum
+      super value
+    else
+      super Hash[STORES.map.with_index.to_a][value.to_sym]
+    end
+  end
 end
